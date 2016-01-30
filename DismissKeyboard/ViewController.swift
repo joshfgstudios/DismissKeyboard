5e89ff279353774013491ecafce80a682b735bf5
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-01-30 17:00:14 +1100

    Initial commit

--- DismissKeyboard/ViewController.swift
+++ DismissKeyboard/ViewController.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        txtInput.delegate = self
+        addDoneButtonToKeyboard()
+    }
+    
+    func addDoneButtonToKeyboard() {
+        let doneToolbar = UIToolbar(frame: CGRectMake(0, 0, 400, 35))
+        doneToolbar.barStyle = UIBarStyle.Default
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        let done = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action: Selector("dismissKeyboard"))
+        
+        var items = [AnyObject]()
+        items.append(flexSpace)
+        items.append(done)
+        doneToolbar.items = items as? [UIBarButtonItem]
+        
+        self.txtInput.inputAccessoryView = doneToolbar
+        
+    }
+    
+    func dismissKeyboard() {
+        self.view.endEditing(true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
